0a327d916c1939bfda3fd61f4b9d0e166e87058b
Ryan Andri <ryanandri@linuxmail.org>
2019-04-21 17:19:13 +0700

    santoni: Carbon ROM bring up

--- carbon_santoni.mk
+++ carbon_santoni.mk
@@ -20,17 +20,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Carbon stuff
+$(call inherit-product, vendor/carbon/config/common.mk)
+
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 # Inherit from santoni device
 $(call inherit-product, device/xiaomi/santoni/device.mk)
+
+# Inherit some common AOSP stuff.
+TARGET_ARCH := arm64
+TARGET_DENSITY := xhdpi
+TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
 PRODUCT_DEVICE := santoni
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_santoni
+PRODUCT_NAME := carbon_santoni
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo carbon_santoni-user
+add_lunch_combo carbon_santoni-userdebug
